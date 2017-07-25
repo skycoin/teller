f28e927bc7fd4cb0450dc3e789817cd1b54e9171
iketheadore <2589926+iketheadore@users.noreply.github.com>
2017-07-26 01:15:54 +0800

    Check if session is closed before closing
    To avoid close closed session panic

--- src/service/session.go
+++ src/service/session.go
@@ -1,21 +1,29 @@
 package service
 
 import (
+	"sync"
 	"time"
 
 	"github.com/skycoin/teller/src/daemon"
 )
 
 type session struct {
+	sync.Mutex
+	closed bool
 	*daemon.Session
 	pingTicker *time.Ticker // ping ticker for sending ping message periodically
 	pongTimer  *time.Timer  // pong timer used to check if the service can receive pong message in specific duration
 }
 
 func (s *session) close() {
+	s.Lock()
+	s.closed = true
+	s.Unlock()
+
 	if s.Session != nil {
 		s.Close()
 	}
+
 	if s.pingTicker != nil {
 		s.pingTicker.Stop()
 		s.pingTicker = nil
@@ -25,6 +33,12 @@ func (s *session) close() {
 		s.pongTimer.Stop()
 		s.pongTimer = nil
 	}
+}
+
+func (s *session) isClosed() bool {
+	s.Lock()
+	defer s.Unlock()
+	return s.closed
 }
 
 func (s *session) resetPongTimer(d time.Duration) {
