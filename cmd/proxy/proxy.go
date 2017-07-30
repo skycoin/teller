f8ea8a40fa0eda852a87954f83bd07db061e06f7
iketheadore <2589926+iketheadore@users.noreply.github.com>
2017-07-30 17:45:50 +0800

    Don't persist private key for proxy, it's not safe

--- cmd/proxy/proxy.go
+++ cmd/proxy/proxy.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"os/user"
@@ -11,11 +10,6 @@ import (
 
 	"path/filepath"
 
-	"encoding/json"
-
-	"bytes"
-	"io/ioutil"
-
 	"github.com/google/gops/agent"
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/teller/src/daemon"
@@ -23,29 +17,13 @@ import (
 	"github.com/skycoin/teller/src/proxy"
 )
 
-var (
-	appDir         = ".teller-proxy"
-	privateKeyFile = "key.json"
-)
-
-type privateKey struct {
-	PrivateKey string `json:"private_key"`
-}
-
 func main() {
 	proxyAddr := flag.String("teller-proxy-addr", "0.0.0.0:7070", "teller proxy listen address")
 	httpAddr := flag.String("http-service-addr", "localhost:7071", "http api service address")
-	newKey := flag.Bool("new-key", false, "generate new key pairs")
 	debug := flag.Bool("debug", false, "debug mode will show more logs")
 	flag.Parse()
 
 	log := logger.NewLogger("", *debug)
-
-	appDir, err := createAppDirIfNotExist(appDir)
-	if err != nil {
-		log.Println("Create app dir failed:", err)
-		return
-	}
 
 	// start gops agent, for profilling
 	if err := agent.Listen(&agent.Options{
@@ -55,27 +33,9 @@ func main() {
 		return
 	}
 
-	pkPath := filepath.Join(appDir, privateKeyFile)
-	var lseckey cipher.SecKey
-	secKey := loadPrivatekey(pkPath)
-	if secKey != nil && !(*newKey) {
-		lseckey, err = cipher.SecKeyFromHex(secKey.PrivateKey)
-		if err != nil {
-			log.Println("Load seckey from hex string failed:", err)
-			return
-		}
-
-		pubkey := cipher.PubKeyFromSecKey(lseckey)
-		log.Println("Pubkey:", pubkey.Hex())
-	} else {
-		_, lseckey = cipher.GenerateKeyPair()
-		pubkey := cipher.PubKeyFromSecKey(lseckey).Hex()
-		log.Println("Pubkey:", pubkey)
-		if err := persistPrivateKey(lseckey.Hex(), pkPath); err != nil {
-			log.Println("Persist pubkey failed:", err)
-			return
-		}
-	}
+	_, lseckey := cipher.GenerateKeyPair()
+	pubkey := cipher.PubKeyFromSecKey(lseckey).Hex()
+	log.Println("Pubkey:", pubkey)
 
 	auth := &daemon.Auth{
 		LSeckey: lseckey,
@@ -124,35 +84,4 @@ func createAppDirIfNotExist(app string) (string, error) {
 		}
 	}
 	return path, nil
-}
-
-func loadPrivatekey(pkfile string) *privateKey {
-	if _, err := os.Stat(pkfile); os.IsNotExist(err) {
-		return nil
-	}
-
-	v, err := ioutil.ReadFile(pkfile)
-	if err != nil {
-		fmt.Println("Read pubkey file failed:", err)
-		return nil
-	}
-	var pk privateKey
-	if err := json.NewDecoder(bytes.NewReader(v)).Decode(&pk); err != nil {
-		fmt.Println("Decode pubkey value failed:", err)
-		return nil
-	}
-	return &pk
-}
-
-func persistPrivateKey(key string, path string) error {
-	v, err := json.MarshalIndent(privateKey{PrivateKey: key}, "", "    ")
-	if err != nil {
-		return fmt.Errorf("encode pubkeyValue failed: %v", err)
-	}
-
-	if err := ioutil.WriteFile(path, v, 0700); err != nil {
-		return fmt.Errorf("write pubkey to file failed: %v", err)
-	}
-
-	return nil
 }
