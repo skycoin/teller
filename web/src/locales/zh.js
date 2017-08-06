05ba8e0f5c6c59d9a9caca9ae226b8578db2f832
hanyouhong <python@sina.cn>
2017-08-06 21:47:23 +0800

    local-zh

--- web/src/locales/zh.js
+++ web/src/locales/zh.js
@@ -12,14 +12,23 @@ export default {
 </ul>
 
 <p>您可以通过输入您的天空币地址并点击下面的"<strong>检查状态</strong>"来核实订单的状态</p>
+<p>每次当您点击<strong>获取地址</strong>, 系统会产生一个新的比特币地址, 一个天空币地址最多只准许兑换5个比特币</p>
     `,
-    btcAddressFor: '给天空币地址{skyAddress}充值的比特币地址是:',
     statusFor: '天空币地址{skyAddress}的订单状态',
     enterAddress: '输入天空币地址',
     getAddress: '获取地址',
     checkStatus: '检查状态',
+    loading: '加载中...',
+    btcAddress: '比特币地址',
     errors: {
       noSkyAddress: '请输入您的天空币地址',
     },
+    statuses: {
+      done: '交易 {id}: 天空币已经发送并确认(更新于{updated}).',
+      waiting_deposit: '交易 {id}: 等待比特币存入(更新于 {updated}).',
+      waiting_send: '交易 {id}: 比特币存入已确认; 天空币发送在队列中 (更新于 {updated}).',
+      waiting_confirm: '交易 {id}: 天空币已发送,等待交易确认 (更新于 {updated}).',
+    },
   },
 };
+
