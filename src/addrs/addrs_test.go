5efc182161bd8112ac0463dbf2a087b2fd04c334
liuguirong <liuguirong@kingsoft.com>
2017-11-27 19:26:00 +0800

    add ethernum address unit test

--- src/addrs/addrs_test.go
+++ src/addrs/addrs_test.go
@@ -86,3 +86,83 @@ func TestNewAddress(t *testing.T) {
 	require.Error(t, err)
 	require.Equal(t, ErrDepositAddressEmpty, err)
 }
+
+func TestNewEthAddrs(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	addresses := []string{
+		"0x12bc2e62a27f8940c373ef1edef7b615aeb045f3",
+		"0x3e0081aa902a21ff8db61b29c05889a3d1b34f45",
+		"0x50e0c87ef74079650ae6cd4ee895f8e1b02714cf",
+	}
+
+	log, _ := testutil.NewLogger(t)
+	etha, err := NewAddrs(log, db, addresses, "test_bucket_eth")
+	require.NoError(t, err)
+
+	addrMap := make(map[string]struct{}, len(etha.addresses))
+	for _, a := range etha.addresses {
+		addrMap[a] = struct{}{}
+	}
+
+	for _, addr := range addresses {
+		_, ok := addrMap[addr]
+		require.True(t, ok)
+	}
+}
+
+func TestNewEthAddress(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	addresses := []string{
+		"0xc0a51efd9c319dd60d93105ab317eb362017ecb9",
+		"0x3f9f942b8bd4f69432c053eef77cd84fd46b8d76",
+		"0x5405f65a71342609249bb347505a4029c85ee88b",
+		"0x01db29b6d512902aa82571267609f14187aa8aa8",
+		"0x01db29b6d512902aa82571267609f14187aa8aa8",
+	}
+
+	log, _ := testutil.NewLogger(t)
+	etha, err := NewAddrs(log, db, addresses, "test_bucket_eth")
+	require.NoError(t, err)
+
+	addr, err := etha.NewAddress()
+	require.NoError(t, err)
+
+	addrMap := make(map[string]struct{})
+	for _, a := range etha.addresses {
+		addrMap[a] = struct{}{}
+	}
+
+	// check if the addr still in the address pool
+	_, ok := addrMap[addr]
+	require.False(t, ok)
+
+	// check if the addr is in used storage
+	used, err := etha.used.IsUsed(addr)
+	require.NoError(t, err)
+	require.True(t, used)
+
+	log, _ = testutil.NewLogger(t)
+	etha1, err := NewAddrs(log, db, addresses, "test_bucket_eth")
+	require.NoError(t, err)
+
+	for _, a := range etha1.addresses {
+		require.NotEqual(t, a, addr)
+	}
+
+	used, err = etha1.used.IsUsed(addr)
+	require.True(t, used)
+
+	// run out all addresses
+	for i := 0; i < 3; i++ {
+		_, err = etha1.NewAddress()
+		require.NoError(t, err)
+	}
+
+	_, err = etha1.NewAddress()
+	require.Error(t, err)
+	require.Equal(t, ErrDepositAddressEmpty, err)
+}
---
+++ src/addrs/eth_test.go
@@ -0,0 +1,116 @@
+package addrs
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/skycoin/teller/src/util/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewETHAddrsAllValid(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	log, _ := testutil.NewLogger(t)
+
+	addressesJson := `{
+    "eth_addresses": [
+		"0xc0a51efd9c319dd60d93105ab317eb362017ecb9",
+		"0x3f9f942b8bd4f69432c053eef77cd84fd46b8d76",
+		"0x5405f65a71342609249bb347505a4029c85ee88b",
+		"0x01db29b6d512902aa82571267609f14187aa8aa8"
+    ]
+}`
+
+	ethAddrMgr, err := NewETHAddrs(log, db, bytes.NewReader([]byte(addressesJson)))
+
+	require.Nil(t, err)
+	require.NotNil(t, ethAddrMgr)
+}
+
+func TestNewBtcAddrsContainsInvalid(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	log, _ := testutil.NewLogger(t)
+
+	addressesJson := `{
+    "eth_addresses": [
+		"0xc0a51efd9c319dd60d93105ab317eb362017ecb9",
+		"0x3f9f942b8bd4f69432c053eef77cd84fd46b8d76",
+		"0x5405f65a71342609249bb347505a4029c85ee88b",
+		"0x01db29b6d512902aa82571267609f14187aa8aa8",
+        "bad"
+    ]
+}`
+
+	expectedErr := errors.New("Invalid deposit address `bad`: Invalid address length")
+
+	ethAddrMgr, err := NewETHAddrs(log, db, bytes.NewReader([]byte(addressesJson)))
+
+	require.Error(t, err)
+	require.Equal(t, expectedErr, err)
+	require.Nil(t, ethAddrMgr)
+}
+
+func TestNewBtcAddrsContainsDuplicated(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	log, _ := testutil.NewLogger(t)
+
+	addressesJson := `{
+      "eth_addresses": [
+		"0xc0a51efd9c319dd60d93105ab317eb362017ecb9",
+		"0x3f9f942b8bd4f69432c053eef77cd84fd46b8d76",
+		"0xc0a51efd9c319dd60d93105ab317eb362017ecb9",
+		"0x01db29b6d512902aa82571267609f14187aa8aa8"
+    ]
+}`
+
+	expectedErr := errors.New("Duplicate deposit address `0xc0a51efd9c319dd60d93105ab317eb362017ecb9`")
+
+	ethAddrMgr, err := NewETHAddrs(log, db, bytes.NewReader([]byte(addressesJson)))
+
+	require.Error(t, err)
+	require.Equal(t, expectedErr, err)
+	require.Nil(t, ethAddrMgr)
+}
+
+func TestNewETHAddrsContainsNull(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	log, _ := testutil.NewLogger(t)
+
+	addressesJson := `{
+      "eth_addresses": []
+}`
+
+	expectedErr := errors.New("No ETH addresses")
+
+	ethAddrMgr, err := NewETHAddrs(log, db, bytes.NewReader([]byte(addressesJson)))
+
+	require.Error(t, err)
+	require.Equal(t, expectedErr, err)
+	require.Nil(t, ethAddrMgr)
+}
+
+func TestNewETHAddrsBadFormat(t *testing.T) {
+	db, shutdown := testutil.PrepareDB(t)
+	defer shutdown()
+
+	log, _ := testutil.NewLogger(t)
+
+	addressesJson := ``
+
+	expectedErr := errors.New("Decode loaded address json failed: EOF")
+
+	ethAddrMgr, err := NewETHAddrs(log, db, bytes.NewReader([]byte(addressesJson)))
+
+	require.Error(t, err)
+	require.Equal(t, expectedErr, err)
+	require.Nil(t, ethAddrMgr)
+}
