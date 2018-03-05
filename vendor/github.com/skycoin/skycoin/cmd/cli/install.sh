7a374d369f5a5b74104585964f9acc6ac194b5f7
Brian Caine <brian.d.caine@gmail.com>
2018-03-04 21:44:53 -0500

    Pruned unused packages

--- vendor/github.com/skycoin/skycoin/cmd/cli/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-go build
-mv cli $GOPATH/bin/skycoin-cli
--- vendor/github.com/skycoin/skycoin/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "skycoin binary dir:" "$DIR"
-pushd "$DIR" >/dev/null
-go run cmd/skycoin/skycoin.go --gui-dir="${DIR}/src/gui/static/" $@
-popd >/dev/null
