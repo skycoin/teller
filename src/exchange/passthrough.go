67442c09532f5aa74dd91abba59ce1d4851719c6
gz-c <token@protonmail.com>
2018-03-11 13:59:10 +0800

    Calculate the appropriate amount to buy from exchange

--- src/exchange/passthrough.go
+++ src/exchange/passthrough.go
@@ -1,6 +1,7 @@
 package exchange
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -10,6 +11,8 @@ import (
 	"github.com/skycoin/exchange-api/db"
 	"github.com/skycoin/exchange-api/exchange"
 	c2cx "github.com/skycoin/exchange-api/exchange/c2cx.com"
+	"github.com/skycoin/skycoin/src/util/droplet"
+	"github.com/skycoin/skycoin/src/visor"
 
 	"github.com/skycoin/teller/src/config"
 	"github.com/skycoin/teller/src/scanner"
@@ -212,13 +215,12 @@ func (p *Passthrough) handleDepositInfoState(di DepositInfo) (DepositInfo, error
 	}
 }
 
-/* BEGIN PSEUDOCODE FOR EXCHANGE PURCHASE LOGIC */
-
 // checkBalance checks that enough coins are held on the exchange
 func (p *Passthrough) checkBalance(di DepositInfo) error {
 	quantity, err := p.exchangeClient.GetBalance(di.CoinType)
 
 	if err != nil {
+		p.log.WithField("depositInfo", di).WithError(err).Error("Failed to get balance from exchange")
 		return err
 	}
 
@@ -254,8 +256,56 @@ func (p *Passthrough) getCheapestAsk(di DepositInfo) (*exchange.MarketOrder, err
 }
 
 // placeOrder places an order on the exchange and returns the orderID
-func (p *Passthrough) placeOrder(di DepositInfo, ask *exchange.MarketOrder) (int, error) {
-	return p.exchangeClient.Buy(fmt.Sprintf("SKY_%s", di.CoinType), ask.Price, ask.Volume)
+func (p *Passthrough) placeOrder(di DepositInfo, ask *exchange.MarketOrder) (int, *exchange.MarketOrder, error) {
+	order, err := buildOrderFromDeposit(di, ask)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	orderID, err := p.exchangeClient.Buy(fmt.Sprintf("SKY_%s", di.CoinType), order.Price, order.Volume)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return orderID, order, nil
+}
+
+// buildOrderFromDeposit creates an order given a deposit and an ask order.
+// The order's volume will based upon the amount of BTC to spend on the order,
+// given the fixed price of the input ask order.
+func buildOrderFromDeposit(di DepositInfo, ask *exchange.MarketOrder) (*exchange.MarketOrder, error) {
+	// Calculate the cost of the ask order in BTC
+	// If the amount available to spend is less than the cost of the order,
+	// adjust the volume of the ask order to match it.
+	if di.DepositValue <= di.Passthrough.DepositValueSpent {
+		return nil, errors.New("deposit has been entirely spent")
+	}
+
+	cost := askCost(ask)
+	available := decimal.New(di.DepositValue-di.Passthrough.DepositValueSpent, -int32(SatoshiExponent))
+
+	// We can afford the entire order, return it unmodified
+	if available.GreaterThan(cost) {
+		return ask, nil
+	}
+
+	// We will bid on part of the order, adjust the volume downward
+	volume := available.Div(ask.Price)
+
+	// Truncate the volume to the maximum decimals spendable by Skycoin
+	volume = volume.Truncate(int32(visor.MaxDropletPrecision))
+
+	return &exchange.MarketOrder{
+		Price:  ask.Price,
+		Volume: volume,
+	}, nil
+}
+
+// Calculates the cost of an ask order
+// TODO -- make this a method of exchange.MarketOrder
+// exchange.MarketOrder should also specify the currency types involved
+func askCost(ask *exchange.MarketOrder) decimal.Decimal {
+	return ask.Price.Mul(ask.Volume)
 }
 
 // checkOrder returns the status of an order
@@ -287,7 +337,10 @@ func (p *Passthrough) fillOrder(di DepositInfo) (DepositInfo, error) {
 	// TODO -- determine fatal/retry cases
 	// TODO -- API wrapper around exchange-api
 
+	log := p.log.WithField("depositInfo", di)
+
 	if err := p.checkBalance(di); err != nil {
+		log.WithError(err).Error("checkBalance failed")
 		return di, err
 	}
 
@@ -296,22 +349,33 @@ beginOrderLoop:
 		// Clear any pending orders by cancelling them
 		// TODO -- if any orders actually ended up completed, match the orderID
 		// with the deposit that made them, and update the deposit
+		// TODO -- find out what happens when cancelling an order that already completed
 		if err := p.clearOrders(); err != nil {
+			log.WithError(err).Error("clearOrders failed")
 			return di, err
 		}
 
 		// Get the cheapest ask bid
 		ask, err := p.getCheapestAsk(di)
 		if err != nil {
+			log.WithError(err).Error("getCheapestAsk failed")
 			return di, err
 		}
 
 		// Place an order matching this ask bid
 		// TODO -- adjust amount based upon remaining BTC to spend
-		orderID, err := p.placeOrder(di, ask)
+		orderID, ask, err := p.placeOrder(di, ask)
 		if err != nil {
+			log.WithError(err).Error("placeOrder failed")
 			return di, err
 		}
+
+		log.WithFields(logrus.Fields{
+			"orderID":   orderID,
+			"askPrice":  ask.Price,
+			"askVolume": ask.Volume,
+			"askCost":   askCost(ask),
+		}).Info("Placed order")
 
 		// Wait for order to complete
 		// If not completed after checkOrderWait, cancel it
@@ -323,8 +387,14 @@ beginOrderLoop:
 		case <-time.After(checkOrderWait):
 			status, err = p.checkOrder(orderID)
 			if err != nil {
+				log.WithError(err).Error("checkOrder failed")
 				return di, err
 			}
+
+			log.WithFields(logrus.Fields{
+				"orderID":     orderID,
+				"orderStatus": status,
+			}).Info("Got order status")
 
 			switch status {
 			case exchange.Completed:
@@ -333,23 +403,43 @@ beginOrderLoop:
 			}
 		}
 
+		// Calculate the number of SKY bought, measured in droplets
+		skyBought := ask.Volume.Mul(decimal.New(droplet.Multiplier, 0)).IntPart()
+		if skyBought < 0 {
+			err := errors.New("Calculated SKY bought is negative")
+			log.WithError(err).Error("CRITICAL ERROR")
+			return di, err
+		}
+
+		// Calculate number of satoshis spent
+		// TODO -- review the math to make sure the truncation caused by IntPart()
+		// does not lead us to have a tiny unspendable amount of BTC remaining
+		// that prevents us from exiting the loop.
+		// A skycoin droplet has a minimum price measured in BTC -- this may be unavoidable
+		// and the loop check may have to account for this possibility.
+		// Also, we only purchase skycoin to a precision that we are able to send,
+		// defined by visor.MaxDropletPrecision, which is 3 at the time of this writing.
+		// So the smallest purchasable unit is 0.001 SKY.
+		// If the BTC/SKY price is 0.0012, then the minimum amount of BTC to spend
+		// is 0.0012 * 0.001 = 0.0000012 BTC, or 120 satoshis.
+		spent := askCost(ask).Mul(decimal.New(SatoshisPerBTC, 0)).IntPart()
+
 		// Update deposit info
 		// TODO -- use UpdateDepositInfoCallback, in case the DB save fails?
 		di, err = p.store.UpdateDepositInfo(di.DepositID, func(di DepositInfo) DepositInfo {
 			di.Passthrough.Orders = append(di.Passthrough.Orders, PassthroughOrder{})
-			// di.Passthrough.SkyBought += o.Amount
-			// di.Passthrough.DepositValueSpent += o.Amount * o.Price
+			di.Passthrough.SkyBought += uint64(skyBought)
+			di.Passthrough.DepositValueSpent += spent
 			return di
 		})
 		if err != nil {
+			log.WithError(err).Error("Attempted to save passthrough order: UpdateDepositInfo failed")
 			return di, err
 		}
 	}
 
 	return di, nil
 }
-
-/* END PSEUDOCODE FOR EXCHANGE PURCHASE LOGIC */
 
 func (p *Passthrough) setStatus(err error) {
 	defer p.statusLock.Unlock()
