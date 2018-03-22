285e83e95383fa35c680d3dabf7d9d59c96cf157
delirium <rmn@mdl.wtf>
2018-03-22 18:04:14 +0800

    fixed waves tests for addrs

--- src/addrs/waves_test.go
+++ src/addrs/waves_test.go
@@ -16,13 +16,11 @@ func TestNewWAVESAddrsAllValid(t *testing.T) {
 
 	log, _ := testutil.NewLogger(t)
 
-	addressesJSON := `{
-    "waves_addresses": [
-		"3PJaDyprvekvPXPuAtxrapacuDJopgJRaU3",
-		"3PFTGLDvE7rQfWtgSzBt7NS4NXXMQ1gUufs",
-		"3P9dUze9nHRdfoKhFrZYKdsSpwW9JoE6Mzf"
-    ]
-}`
+	addressesJSON := `
+3PJaDyprvekvPXPuAtxrapacuDJopgJRaU3
+3PFTGLDvE7rQfWtgSzBt7NS4NXXMQ1gUufs
+3P9dUze9nHRdfoKhFrZYKdsSpwW9JoE6Mzf
+`
 
 	wavesAddrMgr, err := NewWAVESAddrs(log, db, bytes.NewReader([]byte(addressesJSON)))
 
@@ -36,12 +34,10 @@ func TestNewWAVESAddrsContainsInvalid(t *testing.T) {
 
 	log, _ := testutil.NewLogger(t)
 
-	addressesJSON := `{
-    "waves_addresses": [
-		"3P28Lsv1Pxf63EnvqoymXwbhQ1GnFFH5s6C",
-        "bad"
-    ]
-}`
+	addressesJSON := `
+3P28Lsv1Pxf63EnvqoymXwbhQ1GnFFH5s6C
+bad
+`
 
 	expectedErr := errors.New("Invalid deposit address `bad`: Invalid address length")
 
@@ -58,14 +54,12 @@ func TestNewWAVESAddrsContainsDuplicated(t *testing.T) {
 
 	log, _ := testutil.NewLogger(t)
 
-	addressesJSON := `{
-      "waves_addresses": [
-		"3PEruAtC1edYhUPNoNAerP5xjdVaQMDHkPP",
-		"3P3Y5U5CbJNHfszLa9JUieR91Et14yuLsRs",
-		"3P5DKriHBPkeUN1GJXsq5S2tPwXqxw2f1Nr",
-		"3P5DKriHBPkeUN1GJXsq5S2tPwXqxw2f1Nr"
-    ]
-}`
+	addressesJSON := `
+3PEruAtC1edYhUPNoNAerP5xjdVaQMDHkPP
+3P3Y5U5CbJNHfszLa9JUieR91Et14yuLsRs
+3P5DKriHBPkeUN1GJXsq5S2tPwXqxw2f1Nr
+3P5DKriHBPkeUN1GJXsq5S2tPwXqxw2f1Nr
+`
 
 	expectedErr := errors.New("Duplicate deposit address `3P5DKriHBPkeUN1GJXsq5S2tPwXqxw2f1Nr`")
 
@@ -82,28 +76,9 @@ func TestNewWAVESAddrsContainsNull(t *testing.T) {
 
 	log, _ := testutil.NewLogger(t)
 
-	addressesJSON := `{
-      "waves_addresses": []
-}`
-
-	expectedErr := errors.New("No WAVES addresses")
-
-	wavesAddrMgr, err := NewWAVESAddrs(log, db, bytes.NewReader([]byte(addressesJSON)))
-
-	require.Error(t, err)
-	require.Equal(t, expectedErr, err)
-	require.Nil(t, wavesAddrMgr)
-}
-
-func TestNewWAVESAddrsBadFormat(t *testing.T) {
-	db, shutdown := testutil.PrepareDB(t)
-	defer shutdown()
-
-	log, _ := testutil.NewLogger(t)
-
 	addressesJSON := ``
 
-	expectedErr := errors.New("Decode loaded address json failed: EOF")
+	expectedErr := errors.New("No WAVES addresses")
 
 	wavesAddrMgr, err := NewWAVESAddrs(log, db, bytes.NewReader([]byte(addressesJSON)))
 
